d680a5040977f2e5c37b203c17a04da24466dba5
Jeremias <jeremias.reis@symphony.com>
2017-07-31 08:06:38 -0300

    Dockering app

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM docker:latest
+
+WORKDIR /usr/src/devsguide
+
+COPY package.json package-lock.json .
+
+RUN npm install
+
+COPY . .
+
+EXPOSE 8080
+
+CMD [ "npm", "start" ]
--- api/server.js
+++ api/server.js
@@ -6,6 +6,6 @@ const server = restify.createServer();
 
 routes(server);
 
-server.listen(3001, () => console.log('%s listening at %s', server.name, server.url));
+server.listen(8080, () => console.log('%s listening at %s', server.name, server.url));
 
 export default server;
